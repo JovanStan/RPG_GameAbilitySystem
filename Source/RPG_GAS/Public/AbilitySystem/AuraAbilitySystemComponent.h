8dfb2faead6abe1bbc40ddf3939f3debc2e8100c
Jovan <jovanstankovic561@gmail.com>
2025-04-02 14:10:01 +0200

    GAS integrated into project

---
+++ Source/RPG_GAS/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+UCLASS()
+class RPG_GAS_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RPG_GAS/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+
+UCLASS()
+class RPG_GAS_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
